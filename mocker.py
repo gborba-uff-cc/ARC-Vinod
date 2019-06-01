386a14c3d1b3d09f5fcffa4fcc5a3ced99b9d326
Gabriel B <50279580+gborba-uff-cc@users.noreply.github.com>
2019-06-01 16:25:52 -0300

    Update mocker.py
    modificado: docStrings e nomes de parametros nas assinaturas de funções, corrigida a chave que escrevi errada na função setPosAtual, ocorrencias de dict.get(k) para acesso direto dict[k], para evitar o alastramento dos efeitos de receber um None, mensagens do console;
    adicionados: função que abre e carrega um json para a memoria principal e uma para salvar os dados recebidos em um json;
    removidos: alguns passos da inicialização;

--- mocker.py
+++ mocker.py
@@ -36,143 +36,144 @@ def altitude(limiteInf=-20, limiteSup=100):
     return randint(limiteInf, limiteSup)
 
 
-def getPosAtual(dict):
+def getPosAtual(dictPos):
     """
     Posição atual do balão.
 
-    :param dict: Dicionário com as informações de estado do balão.
+    :param dictPos: Dicionário com as informações de estado do balão.
     :return: [latitude, longitude, altura]
     """
-    return [dict.get("latitudeAtual"), dict.get("longitudeAtual"), dict.get("altitudeAtual")]
+    return [dictPos["latitudeAtual"], dictPos["longitudeAtual"], dictPos["alturaAtual"]]
 
 
-def setPosAtual(dict, lat, long, alt):
+def setPosAtual(dictPos, lat, long, alt):
     """
     Modifica a posição atual do balão.
 
-    :param dict: Dicionário com as informações de estado do balão.
+    :param dictPos: Dicionário com as informações de estado do balão.
     :param lat: (int) nova latitude
     :param long: (int) nova longitude
     :param alt: (int) nova altura
     :return: indefinido
     """
-    dict["latitudeAtual"] = lat
-    dict["longitudeAtual"] = long
-    dict["altitudeAtual"] = alt
+    dictPos["latitudeAtual"] = lat
+    dictPos["longitudeAtual"] = long
+    dictPos["alturaAtual"] = alt
 
 
-def getPosicaoDesejada(dict):
+def getPosicaoDesejada(dictServer):
     """
     Posição alvo para o balão.
 
-    :param dict: Dicionário com as informações de localização para o balão.
+    :param dictServer: Dicionário com as infromações de posicionamento enviadas pelo servidor.
     :return: [latitude, longitude, altura]
     """
-    return [dict.get("alvoLatitude"), dict.get("alvoLongitude"), dict.get("alvoAltura")]
+    return [dictServer["alvoLatitude"], dictServer["alvoLongitude"], dictServer["alvoAltura"]]
 
 
-def setPosicaoDesejada(dict, lat, long, alt):
+def setPosicaoDesejada(dictServer, lat, long, alt):
     """
     Modifica a posição alvo para o balão.
 
-    :param dict: Dicionário com as informações de localização para o balão.
+    :param dictServer: Dicionário com as infromações de posicionamento enviadas pelo servidor.
     :param lat: (int) nova latitude
     :param long: (int) nova longitude
     :param alt: (int) nova altura
     :return: indefinido
     """
-    dict["alvoLatitude"] = lat
-    dict["alvoLongitude"] = long
-    dict["alvoAltura"] = alt
+    dictServer["alvoLatitude"] = lat
+    dictServer["alvoLongitude"] = long
+    dictServer["alvoAltura"] = alt
 
 
-def getMaxDesvioAlvo(dict):
+def getMaxDesvioAlvo(dictServer):
     """
-        Desvio máximo aceitável do alvo para o balão.
+    Desvio máximo aceitável do alvo para o balão.
 
-        :param dict: Dicionário com as informações de localização para o balão.
-        :return: [ desvio latitude, desvio longitude, desvio altura]
-        """
-    return [dict.get("desvioLatitude"), dict.get("desvioLongitude"), dict.get("desvioAltura")]
+    :param dictServer: Dicionário com as infromações de posicionamento enviadas pelo servidor.
+    :return: [ desvio latitude, desvio longitude, desvio altura]
+    """
+    return [dictServer["desvioLatitude"], dictServer["desvioLongitude"], dictServer["desvioAltura"]]
 
 
-def setMaxDesvioAlvo(dict, lat, long, alt):
+def setMaxDesvioAlvo(dictServer, lat, long, alt):
     """
     Modifica o desvio mácimo do alvo para o balão.
 
-    :param dict: Dicionário com as informações de localização para o balão.
+    :param dictServer: Dicionário com as infromações de posicionamento enviadas pelo servidor.
     :param lat: (int) nova latitude
     :param long: (int) nova longitude
     :param alt: (int) nova altura
     :return: indefinido
     """
-    dict["desvioLatitude"] = lat
-    dict["desvioLongitude"] = long
-    dict["desvioAltura"] = alt
+    dictServer["desvioLatitude"] = lat
+    dictServer["desvioLongitude"] = long
+    dictServer["desvioAltura"] = alt
 
 
-def setFlagMovimento(dict, val):
+def setFlagMovimento(dictPos, val):
     """
     Modifica a flag que indica que o balão precisa se mover.
 
-    :param dict: Dicionário com as informaçoes de estado do balão.
+    :param dictPos: Dicionário com as informações de estado do balão.
     :param val: (boolean)
     :return: indefinido
     """
-    dict["movimentando"] = val
+    dictPos["movimentando"] = val
 
 
-def getFlagMovimento(dict):
+def getFlagMovimento(dictPos):
     """
     Valor atual do status da movimentação do balao.
 
-    :param dict: Dicionário com as informaçoes de estado do balão, adiciona a chave ao dicionario caso não exista.
+    :param dictPos: Dicionário com as informações de estado do balão.
     :return: (boolean)
     """
-    return dict.get("movimentando")
+    return dictPos["movimentando"]
 
 
-def acionaMotores(dict):
+def acionaMotores(dictPos, dictServer):
     """
     Acionamento e lógica dos motores, forçosamente faz com que o balão passe pela origem da coordenada, o que não é bom
     no caso da logitude precisar ir de -179 para 179 e outras situções parecidas já que fará o balão tomar um caminho
     muito mais longo.
     Funciona com coordenadas com valores inteiros.
 
-    :param dict: Dicionário com as informaçoes de estado do balão.
+    :param dictPos: Dicionário com as informações de estado do balão.
+    :param dictServer: Dicionário com as infromações de posicionamento enviadas pelo servidor.
     :return: indefinido
     """
-    posAtual = getPosAtual(dict)
-    posDesejada = getPosicaoDesejada(dict)
+    posAtual = getPosAtual(dictPos)
+    posDesejada = getPosicaoDesejada(dictServer)
     novaPos = posAtual
     if posAtual[0] != posDesejada[0]:  # latitudes sao diferentes
         if posDesejada[0] > posAtual[0]:  # força a passagem pela latitude zero (nao cruza os polos)
             novaPos[0] = posAtual[0] + 1
-            dict["motorN"] = True
-            dict["motorS"] = False
+            dictPos["motorN"] = True
+            dictPos["motorS"] = False
         else:
             novaPos[0] = posAtual[0] - 1
-            dict["motorN"] = False
-            dict["motorS"] = True
+            dictPos["motorN"] = False
+            dictPos["motorS"] = True
     if posAtual[1] != posDesejada[1]:  # longitude sao diferentes
         if posDesejada[1] > posAtual[1]:  # forca a passagem pela longitude zero e não pela menor distancia (problema)
             novaPos[1] = posAtual[1] + 1
-            dict["motorL"] = True
-            dict["motorO"] = False
+            dictPos["motorL"] = True
+            dictPos["motorO"] = False
         else:
             novaPos[1] = posAtual[1] - 1
-            dict["motorL"] = False
-            dict["motorO"] = True
+            dictPos["motorL"] = False
+            dictPos["motorO"] = True
     if posAtual[2] != posDesejada[2]:  # alturas sao diferentes
         if posDesejada[2] > posAtual[2]:  # forca a passagem pela altura zero
             novaPos[2] = posAtual[2] + 1
-            dict["motorUp"] = True
-            dict["motorDown"] = False
+            dictPos["motorUp"] = True
+            dictPos["motorDown"] = False
         else:
             novaPos[2] = posAtual[2] - 1
-            dict["motorUp"] = False
-            dict["motorDown"] = True
-    setPosAtual(dict, *novaPos)
+            dictPos["motorUp"] = False
+            dictPos["motorDown"] = True
+    setPosAtual(dictPos, *novaPos)
 
 
 def desligaMotores(dict):
@@ -195,7 +196,8 @@ def emPosicao(dictDados, dictServer):
     Verifica se o balão chegou à posição alvo observando a precisão exigida pelo servidor.
     Funciona para coordenadas com valores inteiros.
 
-    :param dict: Dicionário com as informaçoes de estado do balão.
+    :param dictDados: Dicionário com as informaçoes de estado do balão.
+    :param dictServer: Dicionário com as infromações de posicionamento enviadas pelo servidor.
     :return: (boolean)
     """
     posAlvo = getPosicaoDesejada(dictServer)
@@ -226,48 +228,108 @@ def modificacaoDadosPermitida(dictFlags):
     return not dictFlags["lendoDadosPosicionamento"]
 
 
-def leituraPosicionamentoPermitida(dictFlags):
+def carregaJson(nome):
     """
-    Retorna a permissão para a leitura da posição alvo.
+    Tenta abrir o json, até conseguir, para montar um dicionário a partir do mesmo.
+
+    :param nome: nome do arquivo .json.
+    :return: (dict)
+    """
+    while True:
+        try:
+            with open(nome) as arq:
+                return json.load(arq)
+        # não pude acessar
+        except PermissionError:
+            # Está em uso pelo sistema ou o arquivo realmente não tem permissão de leitura
+            pass
+        # quando um arquivo esta sendo salvo (sobrescrito) ele primeiramente é apagado e depois criado o novo
+        # pode ocorrer de quando tentor acessar e arquivo estar sendo escrito por outra local
+        except FileNotFoundError as e:
+            # O arquivo não existe ou "está em uso pelo sistema"
+            pass
+
+def gravaJson(dict, nome):
+    """
+    Monta um dicionário a partir de um arquivo .json.
+
+    :param dict: Dicionário com o que será escrito
+    :param nome: Nome do arquivo .json que será escrito.
+    :return: (dict)
+    """
+    with open(nome, 'w') as arq:
+        json.dump(dict, arq)
+
+
+def sistemaAtivo(dictFlags):
+    """
+    Lê a flag de controle remoto; se o servidor ligou este balão ou não.
 
     :param dictFlags: Dicionário com as flags que o servidor envia para o balão.
     :return: (boolean)
     """
-    return
-
+    return dictFlags["sistema"]
 
 # ============================================================
-with open("route.json", "r") as read_file:
-    route = json.load(read_file)
-
+# REVISAR COM CALMA
+NOME_ARQ_POSICIONAMENTO_SERVER = "posicionamentoServer.json"
+NOME_ARQ_DADOS_POSICIONAMENTO = "dadosPosicionamento.json"
+# sem uso
+# NOME_ARQ_INFOS_REDE = "infosRoteador.json"
+NOME_ARQ_FLAGS_SERVER = "flagsServer.json"
 # inicialização
-getPosAtual(route)
-setPosAtual(route, 0, 0, 0)
-getPosicaoDesejada(route)
-setPosicaoDesejada(route, 0, 0, 0)
-setFlagMovimento(route, False)
-desligaMotores(route)
-
+dadosPos = carregaJson(NOME_ARQ_DADOS_POSICIONAMENTO)
+setFlagMovimento(dadosPos, False)
+desligaMotores(dadosPos)
 while True:
-    if emPosicao(route):
-        setPosicaoDesejada(route, *geraCoordenada())
-        setFlagMovimento(route, True)
-    if getFlagMovimento(route):
-        acionaMotores(route)
-        if emPosicao(route):
-            setFlagMovimento(route, False)
-            desligaMotores(route)
-
-    with open('route.json', 'w') as outfile:
-        json.dump(route, outfile)
-
-    print("Movimento: {mov}\n"
-          "Posição atual: ({posA[0]}, {posA[1]}, {posA[2]})\n"
-          "Posição desejada: ({posD[0]}, {posD[1]}, {posD[2]})\n\n".format(mov=getFlagMovimento(route),
-                                                                           posA=getPosAtual(route),
-                                                                           posD=getPosicaoDesejada(route)))
+    flags = carregaJson(NOME_ARQ_FLAGS_SERVER)
+    if sistemaAtivo(flags):
+        posAlvo = carregaJson(NOME_ARQ_POSICIONAMENTO_SERVER)
+        # rede = carregaJson(NOME_ARQ_INFOS_REDE)
+        # #  [PARA TESTE]
+        # if emPosicao(dadosPos, posAlvo):
+        #     setPosicaoDesejada(posAlvo, *geraCoordenada())
+        #     gravaJson(posAlvo, NOME_ARQ_POSICIONAMENTO_SERVER)
+        # # fim [PARA TESTE]
+        if not emPosicao(dadosPos, posAlvo):
+            setFlagMovimento(dadosPos, True)
+        if getFlagMovimento(dadosPos):
+            acionaMotores(dadosPos, posAlvo)
+            if emPosicao(dadosPos, posAlvo):
+                setFlagMovimento(dadosPos, False)
+                desligaMotores(dadosPos)
+        if modificacaoDadosPermitida(flags):
+            gravaJson(dadosPos, NOME_ARQ_DADOS_POSICIONAMENTO)
+        print("========================================\n"
+              "Flag Sistema    : {f}\n"
+              "-----------------\n"
+              "Movimento       : {mov}\n"
+              "Posição atual   : ({posA[0]}, {posA[1]}, {posA[2]})\n"
+              "Posição desejada: ({posD[0]}, {posD[1]}, {posD[2]})\n"
+              "Desvio permitido: ({desP[0]}, {desP[1]}, {desP[2]})\n"
+              "========================================\n".format(f=sistemaAtivo(flags),
+                                                                  mov=getFlagMovimento(dadosPos),
+                                                                  posA=getPosAtual(dadosPos),
+                                                                  posD=getPosicaoDesejada(posAlvo),
+                                                                  desP=getMaxDesvioAlvo(posAlvo)))
+    else:
+        print("========================================\n"
+              "Flag Sistema    : {}\n"
+              "-----------------\n"
+              "    Sistema em stand-by\n"
+              "========================================\n".format(sistemaAtivo(flags)))
     sleep(1)
 # ==================================================
 
-#Estado Funcional
-#{"sistema": true, "ip": "127.0.0.1", "mascara": "127.0.0.0", "porta": 4500, "status": 200, "dbs": 10, "conectado": "127.0.0.2", "latitude": 2, "longitude": 2, "maxLongitude": 2, "altitude": 1, "maxAltitude": 100,"movimento": false, "latP": 2, "longP": 2, "motorN": false, "motorS": false, "motorL": false, "motorO": false, "motorUp": true, "motorDown": false,"modo": "slave"}
+# Estado funcional dos .json's
+# =>flagsServer
+# {"sistema": true, "lendoDadosPosicionamento": false, "modificandoPosicionamentoAlvo": false,
+# "modificandoInfosRoteador": false}
+# =>posicionamentoServer
+# {"alvoLatitude": 0, "alvoLongitude": 0, "alvoAltura": 0, "desvioLatitude": 0, "desvioLongitude": 0, "desvioAltura": 0}
+# =>dadosPosicionamento
+# {"latitudeAtual": 0, "longitudeAtual": 0, "alturaAtual": 30, "movimentando": true, "motorN": false, "motorS": false,
+# "motorL": false, "motorO": false, "motorUp": false, "motorDown": true, "altitudeAtual": 29}
+# =>infosRoteador
+# {"sistema": true, "conectado": "127.0.0.2", "ip": "127.0.0.1", "mascara": "127.0.0.0", "modo": "slave", "porta": 4500,
+# "status": 200, "dbs": 10}
