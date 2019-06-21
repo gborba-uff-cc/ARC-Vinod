6f5087627af8dac8c70bbde2300febd2bd4fdd35
Gabriel B <50279580+gborba-uff-cc@users.noreply.github.com>
2019-06-21 12:36:00 -0300

    Update s.py
    *adicionado: mesmas instruções que foram acrescentados no Balao/s.py; TODO para a função remove

--- Servidor/src/s.py
+++ Servidor/src/s.py
@@ -1,5 +1,6 @@
 import socket, json
 import threading
+import os
 
 with open("./data/infosRoteador.json", "r") as read_file:
     route = json.load(read_file)
@@ -7,6 +8,10 @@ with open("./data/infosRoteador.json", "r") as read_file:
 class Server():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
     connections = []
+    # nome do arquivo que este server usa para sinalizar ao balao
+    NOME_ARQUIVO_FLAGS_SERVER = os.path.join("data","flagsServer.json")
+    # nome do arquivo que este server passar valores ao balao
+    NOME_ARQUIVO_POSICAO_ALVO = os.path.join("data","posicionamentoServer.json")
     #print("Server pronto...")
     def __init__(self): 
         #self.sock.bind((route["ip"],route["porta"])) 
@@ -21,10 +26,12 @@ class Server():
             
             if not cod:
                 print("D",str(a[0])," ",str(a[1]))
+                # TODO remove precisa de parametro(s)
                 self.connections.remove
             else:
                 import c as cliente
                 import jsonRead as jr
+                from .ManipuladorServerBalao import ManipuladorServerBalao as msb
 
 
 #----------------------------- MENSAGEIROS DE LEITURA (NA VISAO DO SERVIDOR) -------------------------
@@ -32,25 +39,25 @@ class Server():
                     if ("127.0.0.1" != origem):
                         print('master recebeu de volta')
                         cliente.send ('110', info, origem, origem, destinoFinal)
-                        c.close
+                        c.close()
                     else:
                         print('server recebeu de volta')
                         print(info)
                         # escrever aqui no json do servidor
-                        c.close
+                        c.close()
 
                 elif (cod == '120'):  
                     if (destinoFinal == jr.getIp() and jr.getType() == 'slave'): 
                         print('slave recebeu')
                         cliente.send ('110', jr.getLatitude(), jr.getMasterIp(), origem, destinoFinal) #ler location do json
-                        c.close
+                        c.close()
                     elif (destinoFinal == jr.getIp()): #pegar o propio ip
                         cliente.send ('110', jr.getLatitude(), origem, origem, destinoFinal) #ler location do json
-                        c.close
+                        c.close()
                     else:
                         print ('balao mestre recebeu')
                         cliente.send ('120', info, destinoFinal, origem, destinoFinal)
-                        c.close  
+                        c.close()
 #--------------------------------------------------------------------------------------------------------------
 
 #----------------------------- MENSAGEIROS DE ESCRITA ----------------------------------------------------------
@@ -58,31 +65,40 @@ class Server():
                     if ("127.0.0.1" != origem):
                         print('master recebeu de volta')
                         cliente.send ('210', info, origem, origem, destinoFinal)
-                        c.close
+                        c.close()
                     else:
                         print('server recebeu de volta')
                         print(info)
-                        c.close
+                        c.close()
 
                 elif (cod == '220'):  
                     if (destinoFinal == jr.getIp() and jr.getType() == 'slave'): #pegar o propio ip
                         print('slave recebeu')
-                        # TODO escrever no json aqui
-                        #if (testarleitura()):
-                        #   escreverLatitudeJson(info)
-                        cliente.send ('210', 'ok', jr.getMasterIp(), origem, destinoFinal)
-                        c.close
+                        # tenta dizer ao balão que vai atualizar a posição alvo
+                        if msb.atualizaCampoJson(arq=self.NOME_ARQUIVO_FLAGS_SERVER,
+                                                 modificandoPosicionamentoAlvo=True):
+                            # atualizar a posição alvo
+                            msb.atualizaCampoJson(arq=self.NOME_ARQUIVO_POSICAO_ALVO,
+                                                  alvoLatitude=50)
+                            # diz que terminou de atualizar a posição alvo (poderia testar se consegiu dizer)
+                            msb.atualizaCampoJson(arq=self.NOME_ARQUIVO_FLAGS_SERVER,
+                                                  modificandoPosicionamentoAlvo=True)
+                            cliente.send('210', 'ok', jr.getMasterIp(), origem, destinoFinal)
+                        else:
+                            # TODO mensagem de não ok
+                            pass
+                        c.close()
                     elif (destinoFinal == jr.getIp()): #pegar o propio ip
                         cliente.send ('210', jr.getLatitude(), origem, origem, destinoFinal) #ler location do json
-                        c.close
+                        c.close()
                     else:
                         print ('balao mestre recebeu')
                         cliente.send ('220', info, destinoFinal, origem, destinoFinal)
-                        c.close  
+                        c.close()
 #-------------------------------------------------------------------------------------------------------------------------
                 else:
                     print('comando nao existe')
-                    c.close
+                    c.close()
             break
     
     def run(self):
