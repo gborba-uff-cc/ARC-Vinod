e6b6863a330b934352ca33ce47d54c0ca9299a1a
Gabriel B <50279580+gborba-uff-cc@users.noreply.github.com>
2019-06-20 21:56:32 -0300

    Create HandlerServerBalao.py
    *adicionado: classe com metodos para intermediar o server e o balão

---
+++ Balao/src/HandlerServerBalao.py
@@ -0,0 +1,36 @@
+import jsonRead
+import jsonWrite
+import os
+
+
+class HandlerServerBalao():
+
+    def __init__(self):
+        pass
+
+    def atualizaCampoJson(self, arq, **kwargs):
+        """
+        Atualiza o(s) campo(s) no(s) arquivo(s) com o(s) valor(es) que foram recebidos na forma.
+
+        :param arq: nome do arquivo que sera aberto
+        :param kwargs: campo=valor  <- forma de uso;
+                       campo é o parametro que corresponde ao campo que sera atualizado;
+                       valor é o novo valor para o campo;
+        :return: (boolean)
+        """
+        d = jsonRead.carregaJson(arq)
+        if d is not None:
+            for c, v in kwargs.items():
+                d[c] = v
+            jsonWrite.gravaJson(d, arq)
+            return True
+        else:
+            return False
+
+# ------------------------------ TESTE ------------------------------
+# funcionando
+
+# HandlerServerBalao().atualizaCampoJson(arq=os.path.join("data","flagsServer.json"),
+#                                        sistema=False,
+#                                        lendoDadosPosicionamento=False,
+#                                        modificandoPosicionamentoAlvo=False)
